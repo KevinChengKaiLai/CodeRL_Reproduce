a9b687660d833fbd5c1d46cbd193c1be8dd5dc9b
KevinChengKaiLai <144870204+KevinChengKaiLai@users.noreply.github.com>
2026-04-12 23:51:04 -0400

    add run test scripts

---
+++ scripts/sft_actor_benchmark_apps.sh
@@ -0,0 +1,11 @@
+
+
+python generate.py \
+    --model_path models/sft_actor \
+    --tokenizer_path models/codet5-base-tokenizer \
+    --test_path data/APPS/APPS/test/ \
+    --output_path outputs/codes_sft/ \
+    -s 0 -e 500 \
+    --num_seqs 10 \
+    --num_seqs_per_iter 10 \
+    --temperature 0.6
