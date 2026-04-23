9b17176e52a52b405aedcd3e887ad284d22c11b1
KevinChengKaiLai <144870204+KevinChengKaiLai@users.noreply.github.com>
2026-04-22 22:09:28 -0400

    add critic training script

---
+++ scripts/LAB_GPU_REDO/train_critic.sh
@@ -0,0 +1,16 @@
+# Kevin made in  04/22/2026
+
+python train.py \
+    --model codet5-base \
+    --tuning_mode critic \
+    --train-path data/APPS/train/ \
+    --epochs 10  \
+    --lr 5e-5 \
+    --batch-size-per-replica 32 \
+    --grad-acc-steps 8 \
+    --save-freq 1000 \
+    --log-freq 10 \
+    --save_total_limit 2 \
+    --log-freq 10 \
+    --fp16 
+    
